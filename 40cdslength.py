e1efaf3e9ad5ada95cf15774aa04ffbf2df9ffef
xuzhenghua021202 <engxu@ucdavis.edu>
2025-03-03 14:16:28 -0800

    homework

---
+++ 40cdslength.py
@@ -0,0 +1,27 @@
+import gzip
+import sys
+
+import gzip
+import sys
+
+with gzip.open(sys.argv[1], 'rt') as fp:
+    for line in fp:
+        if line[0] != '#':
+            words = line.split()
+            if words[2] == 'CDS':
+                beg = int(words[3])
+                end = int(words[4])
+                print(end - beg + 1)
+ 
+
+with gzip.open(sys.argv[1], 'rt') as fp:
+    for line in fp:
+        if line[0] == '#': 
+        	continue
+        words = line.split()
+        
+        if words[2] != 'CDS': continue 
+       	beg = int(words[3])
+       	end = int(words[4])
+        print(end - beg + 1)
+		
