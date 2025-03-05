e9a2bd03ca6a9bff016ea82f29498d510d93aa77
xuzhenghua021202 <engxu@ucdavis.edu>
2025-03-05 14:02:05 -0800

    homework

--- 33birthday.py
+++ 33birthday.py
@@ -1,25 +1,25 @@
-import random
-
 import sys
+import random
 
 
 trials = int(sys.argv[1])
 days = int(sys.argv[2])
 students = int(sys.argv[3])
-
 shared = 0
 
-for t in range(trials):
-	calendar = [0] * days 
-		
-	for s in range(students):
-		bdays = random.randint(0, days-1)	
-		calendar[bdays] += 1
-	
-	for c in range(len(calendar)):
-		if calendar[c] > 1:
-			shared += 1
-			break 
 
-print(shared / trials)
+for t in range(trials):
+	peeps = []
+	collision = False
 	
+	for i in range(students):
+		bday= random.randint(0, 364)
+		if bday in peeps:
+			collision = True
+			break
+		peeps.append(bday)
+	if collision: 
+		shared += 1
+			
+print(shared/float(trials))
+
