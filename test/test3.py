b6ad8e2d1f3950e21e520efc70c76e8781be56e6
xuzhenghua021202 <engxu@ucdavis.edu>
2025-03-07 13:25:40 -0800

    unit3

--- test/test3.py
+++ test/test3.py
@@ -10,4 +10,23 @@ def is_probdist(probs):
 	return math.isclose(total, 1)
 
 print(is_probdist([0.5, 0.5, 1]))
-		
+
+import sys
+
+alphabet = sys.argv[1]
+match_score = sys.argv[2]
+mismatch_score = sys.argv[3]
+
+print('   ', end='')
+for nt in alphabet: 
+	print(nt, end='  ')
+print()
+
+for C1 in alphabet: 
+	print(C1, end= ' ')
+	for C2 in alphabet: 
+		if C2 == C1:
+			print(match_score, end=' ')
+		else:
+			print(mismatch_score, end=' ')
+	print()
